401e4bf42533ca24cbc696cc8427b304c89706d6
Konstantin <konmitin.work@gmail.com>
2023-09-27 12:00:05 +0300

    fix: удалил лишние комментарии с кодом

--- handlers.py
+++ handlers.py
@@ -17,25 +17,6 @@ router = Router()
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.step1)
-# @router.message(F.data == "step_to_2")
-# async def step_to_2(msg: Message):
-#     await msg.answer(text.step_text1, reply_markup=kb.step2)
-# @router.message(F.data == "link_to_app")
-# @router.message(F.data == "step_to_3")
-# async def step_to_3(msg: Message):
-#     await msg.answer(text.step_text2, reply_markup=kb.step3)
-# @router.message(F.data == "step_to_4")
-# async def step_to_4(msg: Message):
-#     await msg.answer(text.step_text3, reply_markup=kb.step4)
-# @router.message(F.data == "step_to_finished")
-# async def step_to_finished(msg: Message):
-#     await msg.answer(text.step_text4, reply_markup=kb.step_finished)
-# @router.message(F.data == "step_finished")
-# async def step_finished(msg: Message):
-#     await msg.answer(text.menu)
-# @router.message(F.data == "info_company")
-# async def info_company(msg: Message):
-#     await msg.answer(text.menu)
 
 @router.callback_query(F.data == "step_to_2")
 async def step_to_2(clbck: router.callback_query, state: FSMContext):
